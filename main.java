a930053032aa0dafc3aa7ac93fc831d4128d3de2
joseromero050 <jose.romero@insbaixcamp.cat>
2025-04-10 19:18:25 +0200

    commitinicial3

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
 			Biblioteca biblioteca = new Biblioteca();
+            syso
 			Llibre llibre1 = new Llibre("");
 			Llibre llibre2 = new Llibre("");
+                biblioteca.afegirLlibre();
+                biblioteca.afegirLlibre();
