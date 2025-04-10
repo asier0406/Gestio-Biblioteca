261033def069cae9e1bdd92250eb0b71dbfeb5e8
joseromero050 <jose.romero@insbaixcamp.cat>
2025-04-10 18:57:30 +0200

    comitinicial2

---
+++ main.java
@@ -0,0 +1,5 @@
+public class Main {
+	public static void main(String[] args) {
+			Biblioteca biblioteca = new Biblioteca();
+			Llibre llibre1 = new Llibre("");
+			Llibre llibre2 = new Llibre("");
