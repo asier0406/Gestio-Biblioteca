5dfabc6c9cab75669dbe45b58019fc41a9310be4
Arnau Bernat <arnau.bernat@insbaixcamp.cat>
2025-04-25 18:01:07 +0200

    Millora 1 - Historial de préstecs

--- Usuari.java
+++ Usuari.java
@@ -4,12 +4,13 @@ import java.util.List;
 public class Usuari {
     private String nom;
     private List<Llibre> llibresPrestats;
+    private List<Prestec> historialPrestecs;
     private static List<Usuari> usuaris = new ArrayList<>();
     
     public Usuari(String nom) {
         this.nom = nom;
         this.llibresPrestats = new ArrayList<>();
-
+        this.historialPrestecs = new ArrayList<>();
         usuaris.add(this);
     }
 
@@ -52,4 +53,28 @@ public class Usuari {
         }
     }
     public void retornarLlibre(Llibre llibre) { llibresPrestats.remove(llibre); }
+
+    /**
+     * Afegeix un prestec al historial del usuari.
+     * @param Prestec prestec
+     */
+    public void afegirPrestecAlHistorial(Prestec prestec) {
+        historialPrestecs.add(prestec);
+    }
+
+    /**
+     * Mètode per mostar l'historial de prestecs d'un usuari.
+     */
+    public void historialPrestectsUsuari() {
+        System.out.println("\nHistorial de préstecs de " + nom + ":");
+        for (Prestec p : historialPrestecs) {
+            String estat = p.esRetornat() ? "Retornat" : "Pendent";
+            System.out.println(
+                "Llibre: " + p.getLlibre().getTitol() +
+                ", Data préstec: " + p.getDataPrestec() +
+                ", Data retorn: " + p.getDataRetorn() +
+                ", Estat: " + estat
+            );
+        }
+    }
 }
